93e688510d3f7611f740386dd0ff3edb449d4429
iaschnei <ianael.pro@proton.me>
2025-10-06 11:35:51 +0200

    added capture to main

--- srcs/main.c
+++ srcs/main.c
@@ -1,3 +1,4 @@
+#include "capture.h"
 #include "ft_nmap.h"
 #include "packet_store.h"
 
@@ -34,7 +35,16 @@ int main(int ac, char **av) {
 
     task_queue_close(&queue);
 
+    // Initialise packet storage
     packet_store_init(&g_store);
+
+    // Initialise capture
+    t_capture capture;
+    const char *iface = "enp4s0"; //TODO : change that based on ifconfig's result on the VM  
+    if (capture_start(&capture, iface, &config) != 0) {
+        fprintf(stderr, "Failed to start capture\n");
+        return 1;
+    }
 
     if (launch_workers(&queue, &config) != 0) {
         fprintf(stderr, "Error launching workers\n");
@@ -44,6 +54,8 @@ int main(int ac, char **av) {
     }
 
     task_queue_destroy(&queue);
+    capture_stop(&capture);
+    packet_store_destroy(&g_store);
     cleanup_config(&config);
 
     return (0);
